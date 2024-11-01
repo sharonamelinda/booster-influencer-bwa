d9cb229248aff3beb7281417f9bbf44d87a59220
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-11-01 14:22:56 +0700

    Updated index

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,6 @@ module.exports = {
         'booster-darkblue' : '#06071F',
         'booster-blue' : '#1472FF',
         'booster-navy' : '#0A081C',
-        'booster-green' : '#008900',
-        'booster-lightgreen' : '#E8F7E5',
         'booster-grey' : '#969595',
         'booster-light' : '#E9E9E9'
       }
